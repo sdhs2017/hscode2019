1708859c70a3578418a2be150a20fec3eb48d0a7
sdhs2017 <sdhslog2017@163.com>
2019-07-17 17:39:05 +0800

    second commit

---
+++ src/main/java/member/savilio/design_pattern/singleton/test1/Singleton.java
@@ -0,0 +1,20 @@
+package member.savilio.design_pattern.singleton.test1;
+
+
+/**
+ * 单例：
+ */
+public class Singleton {
+
+    public static void main(String[] args){
+        System.out.println("Hello Singleton");
+    }
+
+    /**
+     * test
+     */
+    public void testSingleton(){
+        //TODO
+    }
+
+}

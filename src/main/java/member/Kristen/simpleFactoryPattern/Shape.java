a63ffdf21bb286ec9c2ad998d196c2105e5cccbe
Kristen6765 <jiahui.peng@mail.mcgill.ca>
2019-07-18 14:34:55 +0800

    factory pattern (Jiahui)

--- src/main/java/member/Kristen/simpleFactoryPattern/Shape.java
+++ src/main/java/member/Kristen/simpleFactoryPattern/Shape.java
@@ -1,4 +1,5 @@
 package member.Kristen.simpleFactoryPattern;
+//could use an interface or an abstract class for creating an object
 //defines the interface Shape
 // square, circle, and rectangle class implement Shape
 public interface Shape {

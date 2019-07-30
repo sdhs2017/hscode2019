6657f9df7b272852a43413d3a3a8eb83cf8f8381
sdhs2017 <sdhslog2017@163.com>
2019-07-31 01:12:23 +0800

    add builder pattern by savilio

--- src/main/java/member/savilio/design_pattern/builder/demo3/Client.java
+++ src/main/java/member/savilio/design_pattern/builder/demo3/Client.java
@@ -6,8 +6,8 @@ package member.savilio.design_pattern.builder.demo3;
 public class Client {
 
     public static void main(String[] args) {
-        Product.ConcreteBuilder builder = new Product.ConcreteBuilder("Samsung RAM","Intel CPU");
-        Product product = builder.getProduct();
+        Product.ConcreteBuilder builder = new Product.ConcreteBuilder("id:1001,RAM","id:2001,CPU");
+        Product product = builder.setPart1("Samsung RAM").setPart2("Intel CPU").getProduct();
         System.out.println(product.toString());
     }
 
